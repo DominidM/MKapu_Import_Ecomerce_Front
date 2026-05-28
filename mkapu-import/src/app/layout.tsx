30c1a8a442fa10a10b7142396c035cc85605458b
jere792 <138648408+jere792@users.noreply.github.com>
2026-05-27 22:32:21 -0500

    [27/05/2026] css reclamaciones a global, quitar flash F5

--- mkapu-import/src/app/layout.tsx
+++ mkapu-import/src/app/layout.tsx
@@ -53,6 +53,9 @@ export default function RootLayout({
           rel="apple-touch-icon"
           href="https://res.cloudinary.com/dxuk9bogw/image/upload/w_180,h_180,c_fill/v1767836605/474716814_581641201299027_4444346315622797816_n_karlgu.png"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700;800&family=DM+Sans:wght@400;500&display=swap" rel="stylesheet" />
         <link rel="preconnect" href="https://kodmbciwlfscwtdaejen.supabase.co" />
         <link rel="dns-prefetch" href="https://kodmbciwlfscwtdaejen.supabase.co" />
       </head>
